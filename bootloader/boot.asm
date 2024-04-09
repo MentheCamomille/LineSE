ebff559a6a772adbdeb06d85bbd174c6ca3030c3
MentheCamomille <s.s.1604manganise@gmail.com>
2024-04-09 14:54:29 +0200

    squellette

---
+++ bootloader/boot.asm
@@ -0,0 +1,25 @@
+mov ah, 0x0e
+mov al, 65
+int 0x10
+
+inc al
+int 0x10
+
+inc al
+int 0x10
+
+inc al
+int 0x10
+
+inc al
+int 0x10
+
+inc al
+int 0x10
+
+inc al
+int 0x10
+
+jmp $
+times 510-($-$$) db 0
+db 0x55, 0xaa
