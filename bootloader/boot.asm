14786e752972771b4f49ee7512842bfa80bb5bcb
MentheCamomille <s.s.1604manganise@gmail.com>
2024-04-19 11:44:10 +0200

    change boot.asm (test alphabet)

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,31 +1,15 @@
 mov ah, 0x0e
-mov al, 97
-int 0x10
-
-inc al
-int 0x10
-
-inc al
-int 0x10
-
-inc al
-int 0x10
-
-inc al
-int 0x10
-
 mov al, 65
 int 0x10
 
-inc al
-int 0x10
+loop:
+    inc al
+    cmp al, 'Z' + 1
+    je exit
+    int 0x10
+    jmp loop
+exit:
+    jmp $
 
-inc al
-int 0x10
-
-inc al
-int 0x10
-
-jmp $
 times 510-($-$$) db 0
-db 0x55, 0xaa
+db 0x055, 0x0aa
