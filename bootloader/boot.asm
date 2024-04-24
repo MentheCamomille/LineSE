827aac132cfb012d6be11b2c7615638dbc8b6223
MentheCamomille <s.s.1604manganise@gmail.com>
2024-04-24 10:00:25 +0200

    [ADD] kernel (for test) + zeroes.asm + setup files

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,39 +1,91 @@
-[org 0x7c00]
-mov [BOOT_DISK], dl
+[org 0x7c00]                        
+KERNEL_LOCATION equ 0x1000
+                                    
 
-; setting up the stack
+mov [BOOT_DISK], dl                 
 
-xor ax, ax      ; make sure ds is set to 0
+
+xor ax, ax
 mov es, ax
 mov ds, ax
 mov bp, 0x8000
 mov sp, bp
 
-mov bx, 0x7e00
+mov bx, KERNEL_LOCATION
+mov dh, 2
 
-; reading the disk
-
-mov ah, 2 
-mov al, 1 		; number of sectors to read
-mov ch, 0       ; number of cylinder
-mov dh, 0       ; head number
-mov cl, 2       ; sector number
-mov dl, [BOOT_DISK]     ; drive number to be stored in variable
+mov ah, 0x02
+mov al, dh
+mov ch, 0x00
+mov dh, 0x00
+mov cl, 0x02
+mov dl, [BOOT_DISK]
 int 0x13
 
-; printing what is in the next sector
+mov ah, 0x0
+mov al, 0x3
+int 0x10            ; text mode
 
-mov ah, 0x0e
-mov al, [0x7e00]
-int 0x10
+CODE_SEG equ GDT_code - GDT_start
+DATA_SEG equ GDT_data - GDT_start
+
+cli
+lgdt [GDT_descriptor]
+mov eax, cr0
+or eax, 1
+mov cr0, eax
+jmp CODE_SEG:start_protected_mode
+
 jmp $
+
 BOOT_DISK: db 0
+
+GDT_start:
+    GDT_null:
+        dd 0x0
+        dd 0x0
+
+
+    GDT_code:
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10011010
+        db 0b11001111
+        db 0x0
+
+    GDT_data:
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10010010
+        db 0b11001111
+        db 0x0
+
+
+GDT_end:
+
+GDT_descriptor:
+    dw GDT_end - GDT_start - 1
+    dd GDT_start
+
+[bits 32]
+start_protected_mode:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov ebp, 0x900000       ; 32 bit stack base pointer
+    mov esp, ebp
+
+    jmp KERNEL_LOCATION
+
+
 
 ; magic padding
 
 times 510-($-$$) db 0
 dw 0xaa55
-
-; filling the second sector ( the one that will be readed ) with 'A's
-
-times 512 db 'A'
--- commands.sh
+++ commands.sh
@@ -1,5 +1,14 @@
+# ./setup-gcc-debian.sh
 export PATH=$PATH:/usr/local/i386elfgcc/bin
+
 echo Boot:
 nasm "bootloader/boot.asm" -f bin -o "bins/boot.bin" -i bootloader
+nasm "bootloader/kernel_entry.asm" -f elf -o "bins/kernel_entry.o"
+i386-elf-gcc -ffreestanding -m32 -g -c "bootloader/kernel.cpp" -o "bins/kernel.o"
+nasm "bootloader/zeroes.asm" -f bin -o "bins/zeroes.bin"
 
-qemu-system-x86_64 "bins/boot.bin"
+i386-elf-ld -o "bins/full_kernel.bin" -Ttext 0x1000 "bins/kernel_entry.o" "bins/kernel.o" --oformat binary
+cat "bins/boot.bin" "bins/full_kernel.bin" "bins/zeroes.bin" > "bins/OS.bin"
+
+qemu-system-x86_64 -drive format=raw,file="bins/OS.bin",index=0,if=floppy, -m 128M
+
